625e4fd8089f0b14918364782cc65c3723375bff
gomadango0113 <gomadango0113@gmail.com>
2025-04-07 20:02:41 +0900

    【追加】残りプレイヤーの概念を追加

--- src/main/java/org/gomadango0113/tntrun/manager/GameManager.java
+++ src/main/java/org/gomadango0113/tntrun/manager/GameManager.java
@@ -1,13 +1,22 @@
 package org.gomadango0113.tntrun.manager;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.gomadango0113.tntrun.TNTRun;
 import org.gomadango0113.tntrun.util.ChatUtil;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GameManager {
 
     private static GameStatus status;
     private static int game_time;
+
+    private static final Set<OfflinePlayer> tntrun_players = new HashSet<>();
 
     static {
         status = GameStatus.WAITING;
@@ -30,6 +39,8 @@ public class GameManager {
 
                         if (count_time[0] == 0) {
                             ChatUtil.sendGlobalMessage("ゲーム開始!!");
+                            tntrun_players.addAll(Bukkit.getOnlinePlayers());
+
                             status = GameStatus.RUNNING;
                         }
                         else {
@@ -48,6 +59,10 @@ public class GameManager {
         }
 
         return 1;
+    }
+
+    public Set<OfflinePlayer> getRunPlayers() {
+        return tntrun_players;
     }
 
     public GameStatus getStatus() {
