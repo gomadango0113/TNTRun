b92413f0a18a854eeaf91e200d2e6a75d8d52bb4
gomadango0113 <gomadango0113@gmail.com>
2025-04-10 21:24:21 +0900

    【追加】ItemUtilにキットのLoreを追加

--- src/main/java/org/gomadango0113/tntrun/util/ItemUtil.java
+++ src/main/java/org/gomadango0113/tntrun/util/ItemUtil.java
@@ -1,7 +1,9 @@
 package org.gomadango0113.tntrun.util;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
@@ -29,7 +31,7 @@ public class ItemUtil {
         if (lore != null) {
             get_lore.addAll(lore);
         }
-        setNameLore(itemstack, name, get_lore);
+        setNameLore(itemstack, name, get_lore, true);
 
         return itemstack;
     }
@@ -38,13 +40,63 @@ public class ItemUtil {
         return itemstack;
     }
 
-    private static ItemStack setNameLore(ItemStack itemStack, String name, List<String> lore) {
+    public ItemUtil enableTag(ItemTag itemTag) {
+        if (itemTag == ItemTag.KIT_ITEM) {
+            ItemMeta meta = itemstack.getItemMeta();
+            List<String> lore = meta.getLore() == null ? new ArrayList<>() : new ArrayList<>(meta.getLore());
+            lore.add(ChatColor.GOLD + "キットアイテム");
+            setNameLore(itemstack, meta.getDisplayName(), lore, true);
+
+            return this;
+        }
+
+        return this;
+    }
+
+    public static boolean isItemTag(ItemStack item, ItemTag itemTag) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta != null) {
+            List<String> lore = meta.getLore();
+            if (lore != null) {
+                if (itemTag == ItemTag.KIT_ITEM) {
+                    return lore.contains(ChatColor.GOLD + "キットアイテム");
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static ItemStack setNameLore(ItemStack itemStack, String name, List<String> lore, boolean lore_add) {
         ItemMeta meta = itemStack.getItemMeta();
         if (name != null) meta.setDisplayName(name);
-        if (lore != null) meta.setLore(lore);
+        if (lore_add) {
+            List<String> new_lore = meta.getLore() == null ? new ArrayList<>() : new ArrayList<>(meta.getLore());
+            if (lore != null) new_lore.addAll(lore);
+            meta.setLore(new_lore);
+        }
+        else {
+            if (lore != null) meta.setLore(lore);
+        }
         itemStack.setItemMeta(meta);
 
         return itemStack;
+    }
+
+    public static ItemStack setItemInventory(PlayerInventory inventory, ItemTag itemTag, Material search_item, String name, List<String> lore) {
+        for (ItemStack itemStack : inventory.getContents()) {
+            if (itemStack != null) {
+                ItemMeta meta = itemStack.getItemMeta();
+                if (itemStack.getType() == search_item) {
+                    setNameLore(itemStack, name, lore, true);
+                }
+            }
+        }
+        return null;
+    }
+
+    public enum ItemTag {
+        KIT_ITEM
     }
 
 }
